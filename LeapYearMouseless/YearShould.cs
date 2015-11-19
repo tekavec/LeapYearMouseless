f359f2dd48704f5e1c7f1524387ee5f44000bb9b
Tomaž Tekavec <tomaz@codurance.com>
2015-11-19 16:20:35 +0000

    Test for a year not divisible by 4 passed

--- LeapYearMouseless/YearShould.cs
+++ LeapYearMouseless/YearShould.cs
@@ -5,6 +5,22 @@ namespace LeapYearMouseless
     [TestFixture]
     public class YearShould
     {
+        [Test]
+        public void not_be_a_leap_year_if_not_divisible_by_4()
+        {
+            Assert.IsFalse(new Year(1997).IsLeapYear());
+        }
+    }
 
+    public class Year
+    {
+        public Year(int year)
+        {
+        }
+
+        public bool IsLeapYear()
+        {
+            return false;
+        }
     }
 }
