0bc9ebe0d48d7e0ecc787013fc32cf3fd3380c0d
Tomaž Tekavec <tomaz@codurance.com>
2015-11-24 15:23:49 +0000

    Leap year kata without mouse finished

---
+++ LeapYearMouseless/Year.cs
@@ -0,0 +1,22 @@
+﻿namespace LeapYearMouseless
+{
+    public class Year
+    {
+        private readonly int _year;
+
+        public Year(int year)
+        {
+            _year = year;
+        }
+
+        public bool IsLeapYear()
+        {
+            return IsDivisibleBy(IsDivisibleBy(100) ? 400 : 4);
+        }
+
+        private bool IsDivisibleBy(int value)
+        {
+            return _year%value == 0;
+        }
+    }
+}
---
+++ LeapYearMouseless/YearShould.cs
@@ -0,0 +1,38 @@
+﻿using NUnit.Framework;
+
+namespace LeapYearMouseless
+{
+    [TestFixture]
+    public class YearShould
+    {
+
+        [Test]
+        public void not_be_a_leap_year_if_not_divisible_by_4()
+        {
+            Assert.IsFalse(IsLeapYear(1997));
+        }
+
+        [Test]
+        public void be_a_leap_year_if_divisible_by_4()
+        {
+            Assert.IsTrue(IsLeapYear(1996));
+        }
+
+        [Test]
+        public void be_a_leap_year_if_divisible_by_400()
+        {
+            Assert.IsTrue(IsLeapYear(1600));
+        }
+
+        [Test]
+        public void not_be_a_leap_year_if_divisible_by_100_but_not_by_400()
+        {
+            Assert.IsFalse(IsLeapYear(1800));
+        }
+
+        private static bool IsLeapYear(int year)
+        {
+            return new Year(year).IsLeapYear();
+        }
+    }
+}
